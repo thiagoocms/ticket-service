ba2bec5a10ddd4d5fad3ad628f446f0ebc617c3c
thiagoocms <thiago.mendes@mv.com.br>
2024-01-15 20:47:11 -0300

    reff in users crud

--- src/main/java/br/com/ticketservice/service/UserService.java
+++ src/main/java/br/com/ticketservice/service/UserService.java
@@ -1,78 +1,22 @@
-package br.com.ticketservice.service;
+    package br.com.ticketservice.service;
 
-import br.com.ticketservice.domain.user.User;
-import br.com.ticketservice.dto.user.UserDTO;
-import br.com.ticketservice.repository.UserRepository;
-import br.com.ticketservice.validation.UserValidation;
-import lombok.RequiredArgsConstructor;
-import org.modelmapper.TypeToken;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+    import br.com.ticketservice.dto.user.UserDTO;
+    import org.springframework.data.domain.Page;
+    import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+    import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class UserService extends AbstractService {
+    public interface UserService {
 
-    private final UserRepository userRepository;
-    private final UserValidation userValidation;
+        List<UserDTO> createOrUpdateByList(List<UserDTO> dtoList) throws Throwable;
 
-    public List<UserDTO> createOrUpdateByList(List<UserDTO> dtoList) throws Throwable {
-        List<UserDTO> userArrayList = new ArrayList<>();
-        for (UserDTO dto : dtoList) {
-            if (Objects.isNull(dto.getId())) {
-                userArrayList.add(this.create(dto));
-            } else {
-                userArrayList.add(this.update(dto.getId(), dto));
-            }
-        }
-        return userArrayList;
+        UserDTO create(UserDTO dto) throws Throwable;
+
+        UserDTO update(Long id, UserDTO dto) throws Throwable;
+
+        UserDTO findById(Long id) throws Throwable;
+
+        Page<UserDTO> findByAll(Pageable pageable);
+
+        void delete(Long id) throws Throwable;
     }
-
-    public UserDTO create(UserDTO dto) throws Throwable {
-
-        User user = modelMapper.map(dto, User.class);
-        userValidation.checkOwnerFieldsToCreate(user);
-        userValidation.checkMandatoryFields(user);
-        modelMapper.map(userRepository.save(user), dto);
-
-        return dto;
-    }
-
-    public UserDTO update(Long id, UserDTO dto) throws Throwable {
-
-        User user = modelMapper.map(dto, User.class);
-        userValidation.checkUpdateConsistence(id, user);
-        userValidation.checkMandatoryFields(user);
-        modelMapper.map(userRepository.save(user), dto);
-
-        return dto;
-    }
-
-    public UserDTO findById(Long id) throws Throwable {
-
-        User user = userValidation.checkExistUser(id);
-
-        return modelMapper.map(user, UserDTO.class);
-    }
-
-    public Page<UserDTO> findByAll(Pageable pageable) {
-
-        Page<User> page = this.userRepository.findAllByDeletedIsFalse(pageable);
-        return modelMapper.map(page, new TypeToken<Page<UserDTO>>() {
-        }.getType());
-
-    }
-
-    public void delete(Long id) throws Throwable {
-
-        User user = userValidation.checkExistUser(id);
-        user.setDeleted(Boolean.TRUE);
-        userRepository.save(user);
-
-    }
-}
