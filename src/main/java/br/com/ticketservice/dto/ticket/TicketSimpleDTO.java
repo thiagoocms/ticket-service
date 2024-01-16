d0aa27b0da80187271bd8056e58d894e705c5200
thiagoocms <thiago.mendes@mv.com.br>
2024-01-15 23:30:54 -0300

    created domain for tickets

---
+++ src/main/java/br/com/ticketservice/dto/ticket/TicketSimpleDTO.java
@@ -0,0 +1,36 @@
+package br.com.ticketservice.dto.ticket;
+
+import br.com.ticketservice.dto.category.CategorySimpleDTO;
+import br.com.ticketservice.dto.user.UserDTO;
+import br.com.ticketservice.enumerated.TicketStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TicketSimpleDTO implements Serializable {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private Long userId;
+
+    private CategorySimpleDTO category;
+
+    private UserDTO employee;
+
+    private TicketStatusEnum status;
+
+    private Long waitingDate;
+
+    private Long finishedDate;
+}
---
+++ src/main/java/br/com/ticketservice/enumerated/TicketStatusEnum.java
@@ -0,0 +1,9 @@
+package br.com.ticketservice.enumerated;
+
+public enum TicketStatusEnum {
+
+    NOT_STARTED,
+    IN_PROGRESS,
+    FINISHED,
+    CANCELED
+}
